e172e0ff42ac96af938421a85efed1c57a32c5c8
Chriss Philip Saji <chriss.16hacker@gmail.com>
2025-07-08 15:58:49 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,8 +1,7 @@
-// Google Docs Dark Mode - Content Script
+
 console.log('Google Docs Dark Mode content script loaded');
 
 function injectStyles() {
-    // Find the iframe that contains the document editor
     const editorIframe = document.querySelector('iframe.docs-editors-iframe');
 
     if (editorIframe && editorIframe.contentDocument) {
@@ -11,17 +10,11 @@ function injectStyles() {
         styleElement.rel = 'stylesheet';
         styleElement.type = 'text/css';
         styleElement.href = chrome.runtime.getURL('styles.css');
-        
-        // Inject the link element into the iframe's head
         editorIframe.contentDocument.head.appendChild(styleElement);
         console.log('Styles injected into iframe.');
     } else {
-        // If the iframe isn't ready yet, wait and try again.
-        // Google Docs loads content dynamically.
         console.log('Editor iframe not found, will retry...');
         setTimeout(injectStyles, 500);
     }
 }
-
-// Start the injection process
 injectStyles();
